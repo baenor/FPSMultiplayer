7a043f4468998587c43476022920ed7ca0ed194c
Fabio Runci <Fabio Runci>
2016-04-07 20:25:23 +0200

    ep 3!

---
+++ Episodio 3/Player/PlayerNetwork.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.Characters.FirstPerson;
+
+public class PlayerNetwork : Photon.MonoBehaviour {
+
+    public Camera myCamera;
+    public AudioListener listener;
+
+	// Use this for initialization
+	void Start () {
+        if (photonView.isMine)
+        {
+            myCamera.enabled = true;
+            listener.enabled = true;
+            GetComponent<FirstPersonController>().enabled = true;
+        }
+	}
+
+}
