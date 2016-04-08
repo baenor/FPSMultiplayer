8340e4ec674de0000b0fad0c615bbb0071b34924
Fabio Runci <Fabio Runci>
2016-04-08 21:21:37 +0200

    ep 4!

---
+++ Episodio 4/Player/PlayerShooting.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerShooting : Photon.MonoBehaviour {
+
+    public Transform firePoint;
+    public Transform shellPoint;
+    public ParticleSystem muzzle;
+
+
+    float fireRate = 0.3f;
+    float timePassed = 0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timePassed += Time.deltaTime;
+        if (photonView.isMine && Input.GetMouseButton(0) && timePassed >= fireRate)
+        {
+            Shooting();
+        }
+	}
+
+    void Shooting()
+    {
+        //Istanzia un proiettile 
+        photonView.RPC("InstantiateBullet", PhotonTargets.All, null);
+        timePassed = 0f;
+    }
+
+
+    [PunRPC]
+    void InstantiateBullet()
+    {
+        Instantiate(Resources.Load("BasicBullet"), firePoint.position, firePoint.rotation);
+        Instantiate(Resources.Load("Shell"), shellPoint.position, shellPoint.rotation);
+
+        muzzle.Play();
+    }
+}
