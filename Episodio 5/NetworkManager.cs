516fd8ac0575ebfcf19b4494cd7cf61a79a4015d
Fabio Runci <Fabio Runci>
2016-04-13 14:44:17 +0200

    ep 5

---
+++ Episodio 5/NetworkManager.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NetworkManager : Photon.MonoBehaviour {
+
+    public Transform spawnPoint;
+
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 100), PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+	// Use this for initialization
+	void Start () {
+
+        PhotonNetwork.autoJoinLobby = true;
+        PhotonNetwork.ConnectUsingSettings("0.0001 version bla bla");
+
+	}
+	
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    //Ho fallito la funzione JoinRandomRoom...ovvero no stanza trovata
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", spawnPoint.position, spawnPoint.rotation, 0);
+    }
+
+
+}
